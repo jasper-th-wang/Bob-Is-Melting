9a782d6288f7cdf9004655b7cd943462a337ec9a
Jasper Wang <jasper8777@icloud.com>
2024-03-24 15:29:41 -0700

    Clean up

--- core/src/dev/jasper/game/BobIsMelting.java
+++ core/src/dev/jasper/game/BobIsMelting.java
@@ -1,10 +1,7 @@
 package dev.jasper.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 import dev.jasper.game.screens.PlayScreen;
 
 /**
